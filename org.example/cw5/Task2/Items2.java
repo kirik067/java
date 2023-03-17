c7e42047626e83dfb88e46bbaee1aaf118e3371a
Kirill <tvoag.2015@mail.ru>
2023-03-17 17:35:49 +0500

    JAVA Collections: An Introduction

---
+++ org.example/cw5/Task2/Items2.java
@@ -0,0 +1,15 @@
+package org.example.Task2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Items2 {
+
+    private String name;
+    private String countryName;
+    private Double price;
+    private Double weight;
+    private Integer sort;
+}
---
+++ org.example/cw5/Task3/Books.java
@@ -0,0 +1,19 @@
+package org.example.Task3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Books {
+
+    private String name;
+    private String author;
+    private Double price;
+    private Integer year;
+    private Integer pgNum;
+
+
+
+}
+
